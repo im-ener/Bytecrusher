c11a7dabb1349c9791841d66f36c50da3a35bd72
Shirley69420 <148286534+Shirley69420@users.noreply.github.com>
2024-01-24 00:50:33 -0500

    what i have at 1 o clock in the moring
    ^

---
+++ Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://world.tscn") # change the res://game.tscn to be what the main game is called
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://options_menu.tscn") # change the res://options_menu.tscn to be what the main game is called
+
+
+func _on_quit_pressed():
+	get_tree().quit()
