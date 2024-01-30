9ceb433e70e8b73ec5cd049a3034973aeb14c48a
im-ener <109366507+im-ener@users.noreply.github.com>
2024-01-30 00:57:36 -0500

    game no work

---
+++ HUD.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var armor = $MarginContainer/Stats/Values/ArmorValue
+@onready var health = $MarginContainer/Stats/Values/HealthValue
+@onready var ammo = $MarginContainer/Stats/Ammo/AmmoValue
+
+func _process(delta):
+	var current_gun = PlayerStats.current_gun
+	armor.text = PlayerStats.get_armor()
+	health.text = PlayerStats.get_health()
+	
+	if current_gun == "Pistol":
+		ammo.text = PlayerStats.get_pistol_ammo()
--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -1,21 +1,31 @@
-extends ScriptCreateDialog
+extends Node3D
 
 @onready var gun_sprite = $CanvasLayer/Control/Gunsprite
 @onready var gun_rays = $GunRays.get_children()
 
-var can_shoot = true
 
+var can_shoot = true
+var damage = 8
 func _ready():
 	gun_sprite.play("idle")
 	
 func check_hit():
-	pass
+	for ray in gun_rays:
+		if ray.is_colliding():
+			if ray.get_collider(is_in_group("Enemy")):
+				ray.get.collider().take_damage(damage)
 	
+
+
+
+
 func _process(delta):
-	if Input.is_action_just_pressed("shoot") and can_shoot:
+	if Input.is_action_just_pressed("shoot") and can_shoot and PlayerStats.ammo_pistol > 0:
 		gun_sprite.play("shoot")
 		check_hit()
+		PlayerStats.change_pistol_ammo(-1)
 		can_shoot = false
+		
 		await (gun_sprite)
 		
 		can_shoot = true
