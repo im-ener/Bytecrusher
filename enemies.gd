75c1cdf1b2adaff22393fbe0644319dc6ae8745b
Shirley69420 <148286534+Shirley69420@users.noreply.github.com>
2024-01-30 03:21:04 -0500

    smoke weed
    weeeeeeeeeeeeeeeeee

--- enemies.gd
+++ enemies.gd
@@ -1,19 +1,41 @@
 extends CharacterBody3D
 
-var player = null
+#@onready var nav = get_tree().get_nodes_in_group("NavMesh")[0]
+#@onready var player= get_tree().get_nodes_in_group("Player")[0]
 
-const SPEED = 4.0
-
-@export var player_path : NodePath
-
-@onready var nav_agent = $NavigationAgent3D
+#var path = [] #hold the path coordinate from the enemy to the player
+#var path_index = 0 #keep track of which coordinate to go to
+#var speed = 3 #change if needed
+#var health = 20 #change if needed
 
 func _ready():
-	player = get_node(player)
+	pass
 
-func _process(delta):
-	velocity = Vector3.ZERO
-	nav_agent.set_target_position(player.global_transform.origin)
-	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-	move_and_slide()
+#func take_damage(dmg_amount):
+#	health -= dmg_amount
+#	if health <= 0:
+#		death()
+
+#func _physics_process(_delta):
+#	if path_index < path.size():
+#		var direction = (path[path_index] - global_transform.origin)
+#		if direction.length() <1:
+#			path_index += 1
+#		else:
+#			velocity = direction * speed
+#			move_and_slide()
+	else:
+		find_path(player.global_transform.origin)
+
+func find_path(target):
+	path = nav.get_simple_path(global_transform.origin,target)
+	path_index = 0
+
+func death():
+	set_process(false)
+	set_physics_process(false)
+	$CollisionShape.disabled = true
+	$AnimatedSprite3D.play("die")
+
+func shoot(_target):
+	pass
