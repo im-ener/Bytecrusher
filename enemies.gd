809ab9dc794ed90338d13f6786a6c8d44427b000
im-ener <109366507+im-ener@users.noreply.github.com>
2024-01-30 03:32:21 -0500

    Revert "Merge branch 'main' of https://github.com/im-ener/Bytecrusher"
    This reverts commit 2ad3b1508b201059525edc943d731c6a9c2ae9f1, reversing
    changes made to da4270bd408acdcd53e3b9c74617657235d4f42b.

--- enemies.gd
+++ enemies.gd
@@ -1,41 +1,41 @@
 extends CharacterBody3D
 
-#@onready var nav = get_tree().get_nodes_in_group("NavMesh")[0]
-#@onready var player= get_tree().get_nodes_in_group("Player")[0]
+@onready var nav = get_tree().get_nodes_in_grouping(NavMesh)[0]
+@onready var player = get_tree().get_nodes_in_grouping(player)[0]
 
-#var path = [] #hold the path coordinate from the enemy to the player
-#var path_index = 0 #keep track of which coordinate to go to
-#var speed = 3 #change if needed
-#var health = 20 #change if needed
+var path = []
+var path_index = 0
+var speed = 5
+var health = 20
 
-func _ready():
+func _ready( ):
 	pass
 
-#func take_damage(dmg_amount):
-#	health -= dmg_amount
-#	if health <= 0:
-#		death()
 
-#func _physics_process(_delta):
-#	if path_index < path.size():
-#		var direction = (path[path_index] - global_transform.origin)
-#		if direction.length() <1:
-#			path_index += 1
-#		else:
-#			velocity = direction * speed
-#			move_and_slide()
+func take_damage(dmg_amount):
+	health -= dmg_amount
+	if health <= 0:
+		death();
+
+func _physics_process(delta):
+	if path_index < path.size();
+			var direction = [path[path_index] - global_transform.origin]
+		if direction.length() < 1;
+path_index += 1
 	else:
-		find_path(player.global_transform.origin)
+		move_and_slide(direction.normalized() * speed, Vector3.UP)
+else:
+	find_path(player.global_transform.origin)
 
-func find_path(target):
-	path = nav.get_simple_path(global_transform.origin,target)
+func find_path(target);
+	path = nav.get_simple_path(global_transform.origin.target)
 	path_index = 0
 
-func death():
-	set_process(false)
-	set_physics_process(false)
-	$CollisionShape.disabled = true
-	$AnimatedSprite3D.play("die")
-
-func shoot(_target):
+func death();
+set_process(false)
+set_physics_process(false)
+$CollisionShape.disabled = true;
+$animatedSprite3D.play("die")
+	pass
+func shoot(target);
 	pass
